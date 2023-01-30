3a42583c04345b760b2c50be21bd26cbbe4f00ee
Anirudh Srinivas Vellimedu <73414294+VSAnirudh2509@users.noreply.github.com>
2023-01-30 12:48:21 +0530

    Add files via upload

--- Vectors/12/10.3.13/codes/Vector4a.py
+++ Vectors/12/10.3.13/codes/Vector4a.py
@@ -1,10 +1,7 @@
 import numpy as np
-list1=[1,0]
-list2=[1,0]
-list3=[1,0]
-a = np.array(list1)
-b = np.array(list2)
-c = np.array(list3)
+a = np.array([1,0])
+b = np.array([1,0])
+c = np.array([1,0])
 res=-((a**2+b**2+c**2))/2
 print(res)
 
